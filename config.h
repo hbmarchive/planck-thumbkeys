4cc27e255472b6855c4a7dd30302a48ef0c5a374
Hannah Morrison <hannah@landhmorrison.scot>
2023-09-27 10:31:46 +0100

    Initial commit

---
+++ config.h
@@ -0,0 +1,37 @@
+/*
+Copyright 2020 Pierre Chevalier <pierrechevalier83@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Enable inertia mouse
+#define MOUSEKEY_INERTIA
+
+#define MOUSEKEY_DELAY 100
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_MAX_SPEED 30
+#define MOUSEKEY_TIME_TO_MAX 40
+#define MOUSEKEY_FRICTION 21
+#define MOUSEKEY_MOVE_DELTA 1
+
+#define MOUSEKEY_WHEEL_DELAY 100
+#define MOUSEKEY_WHEEL_INTERVAL 80
+#define MOUSEKEY_WHEEL_MAX_SPEED 6
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+
+// Default tapping term
+#define TAPPING_TERM 190
+#define PERMISSIVE_HOLD
---
+++ keymap.c
@@ -0,0 +1,342 @@
+#include QMK_KEYBOARD_H
+
+// My overloaded layer keys.
+#define LT_NAV_SPACE LT(NAV_LAYER,  KC_SPC)
+#define LT_NUM_ENTER LT(NUM_LAYER,  KC_ENT)
+
+enum my_layers {
+  BASE_LAYER,
+  SYM_LAYER,
+  NAV_LAYER,
+  NUM_LAYER,
+  SCUT_LAYER
+};
+
+enum my_keycodes {
+    M_ALTT = SAFE_RANGE,
+    M_APP1,
+    M_APP2,
+    M_APP3,
+    M_APP4,
+    M_APP5,
+    M_LIKE,
+    M_1PASS,
+    M_HIDE,
+    M_MAX,
+    M_CLOSE,
+    M_NDESK,
+    M_PDESK,
+    M_NTRM,
+    M_EMOJI,
+    M_ISCROS,
+    M_ISWIN
+};
+
+// Stores state of M_ALTT macro - true if we are currently tabbing between
+// windows.
+static bool m_altt_pressed = false;
+
+// Toggle for keys that affect the the desktop - value can be changed in
+// function layer
+static bool m_is_chromebook = false;
+
+// Used to temporarily store the state of the mod keys.
+static uint8_t mod_state = 0;
+
+// State for managing shift backspace behaviour.
+static bool kc_del_registered = false;
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [BASE_LAYER] = LAYOUT_planck_grid(
+    KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,          KC_BRID,  KC_BRIU,  KC_J,          KC_L,            KC_U,           KC_Y,           KC_BSPC,
+    KC_A,           KC_R,           KC_S,           KC_T,           KC_G,          KC_VOLD,  KC_VOLU,  KC_M,          KC_N,            KC_E,           KC_I,           KC_O,
+    KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,          KC_MPLY,  KC_MNXT,  KC_K,          KC_H,            KC_COMM,        KC_DOT,         KC_SLSH,
+    OSM(MOD_LCTL),  OSM(MOD_LALT),  OSM(MOD_LGUI),  OSM(MOD_LSFT),  LT_NAV_SPACE,  KC_TAB,   KC_ESC,   LT_NUM_ENTER,  OSL(SYM_LAYER),  OSM(MOD_LGUI),  OSM(MOD_LALT),  OSM(MOD_LCTL)
+  ),
+
+  [SYM_LAYER] = LAYOUT_planck_grid(
+    KC_EXLM,  LSFT(KC_2),     LSFT(KC_3),  KC_DLR,   KC_PERC,  KC_TRNS,  KC_TRNS,  KC_CIRC,  KC_AMPR,        KC_ASTR,        KC_NO,    KC_NO,
+    KC_GRV,   LSFT(KC_NUBS),  KC_LBRC,     KC_LCBR,  KC_LPRN,  KC_TRNS,  KC_TRNS,  KC_COLN,  LSFT(KC_QUOT),  LSFT(KC_BSLS),  KC_UNDS,  KC_PLUS,
+    KC_CAPS,  KC_NUBS,        KC_RBRC,     KC_RCBR,  KC_RPRN,  KC_TRNS,  KC_TRNS,  KC_SCLN,  KC_QUOT,        KC_BSLS,        KC_MINS,  KC_EQL,
+    KC_TRNS,  KC_TRNS,        KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,        KC_TRNS,  KC_TRNS
+  ),
+
+  [NAV_LAYER] = LAYOUT_planck_grid(
+    KC_PSCR,   KC_MNXT,     KC_MPLY,     KC_VOLU,           KC_BRIU,     KC_TRNS,  KC_TRNS,  KC_NO,            M_PDESK,  LCTL(KC_TAB),  M_ALTT,   M_NDESK,
+    M_ISCROS,  KC_MPRV,     KC_MUTE,     KC_VOLD,           KC_BRID,     KC_TRNS,  KC_TRNS,  KC_WH_U,          KC_LEFT,  KC_DOWN,       KC_UP,    KC_RGHT,
+    M_ISWIN,   LCTL(KC_X),  LCTL(KC_C),  LSFT(LCTL(KC_C)),  LCTL(KC_V),  KC_TRNS,  KC_TRNS,  KC_WH_D,          KC_HOME,  KC_PGDN,       KC_PGUP,  KC_END,
+    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,           KC_TRNS,     KC_TRNS,  KC_ESC,   OSL(SCUT_LAYER),  KC_TRNS,  KC_TRNS,       KC_TRNS,  KC_TRNS
+  ),
+
+  [NUM_LAYER] = LAYOUT_planck_grid(
+    KC_PAST,  KC_1,     KC_2,     KC_3,     KC_PPLS,  KC_TRNS,  KC_TRNS,  KC_NO,    KC_F1,    KC_F2,    KC_F3,    KC_BSPC,
+    KC_PSLS,  KC_4,     KC_5,     KC_6,     KC_PMNS,  KC_TRNS,  KC_TRNS,  KC_NO,    KC_F4,    KC_F5,    KC_F6,    KC_NO,
+    KC_0,     KC_7,     KC_8,     KC_9,     KC_DOT,   KC_TRNS,  KC_TRNS,  KC_NO,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  CW_TOGG,  KC_TAB,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  ),
+
+  [SCUT_LAYER] = LAYOUT_planck_grid(
+    KC_NO,    M_NTRM,   KC_NO,    KC_NO,    M_LIKE,   KC_TRNS,  KC_TRNS,  KC_NO,    M_HIDE,   M_MAX,    M_CLOSE,  KC_NO,
+    M_APP1,   M_APP2,   M_APP3,   M_APP4,   M_APP5,   KC_TRNS,  KC_TRNS,  KC_NO,    KC_NO,    M_EMOJI,  KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    M_1PASS,  KC_NO,    KC_TRNS,  KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  )
+
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+  // Stop pressing the alt key once a key other than the alt-tab macro has been
+  // pressed.
+  if (keycode != M_ALTT && m_altt_pressed) {
+    unregister_code(KC_LALT);
+    m_altt_pressed = false;
+  }
+
+  mod_state = get_mods();
+
+  switch (keycode) {
+    // Shift-backspace produces delete.
+    case KC_BSPC:
+      if (record->event.pressed) {
+        if (mod_state & MOD_MASK_SHIFT) {
+          del_mods(MOD_MASK_SHIFT);
+          register_code(KC_DEL);
+          kc_del_registered = true;
+          set_mods(mod_state);
+          return false;
+        }
+      } else {
+        if (kc_del_registered) {
+          unregister_code(KC_DEL);
+          kc_del_registered = false;
+          return false;
+        }
+      }
+      break;
+    case M_ALTT:
+      if (record->event.pressed) {
+        if (!m_altt_pressed) {
+          register_code(KC_LALT);
+          m_altt_pressed = true;
+        }
+        tap_code(KC_TAB);
+      }
+      break;
+    case M_APP1:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LALT));
+          SEND_STRING(SS_TAP(X_1));
+          SEND_STRING(SS_UP(X_LALT));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_1));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+      }
+      break;
+    case M_APP2:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LALT));
+          SEND_STRING(SS_TAP(X_2));
+          SEND_STRING(SS_UP(X_LALT));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_2));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+      }
+      break;
+    case M_APP3:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LALT));
+          SEND_STRING(SS_TAP(X_3));
+          SEND_STRING(SS_UP(X_LALT));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_3));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+      }
+      break;
+    case M_APP4:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LALT));
+          SEND_STRING(SS_TAP(X_4));
+          SEND_STRING(SS_UP(X_LALT));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_4));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+      }
+      break;
+    case M_APP5:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LALT));
+          SEND_STRING(SS_TAP(X_5));
+          SEND_STRING(SS_UP(X_LALT));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_5));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+      }
+      break;
+    case M_LIKE:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LALT)SS_TAP(X_5)SS_UP(X_LALT));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_5));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+        SEND_STRING(SS_DELAY(100));
+        SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LALT));
+        SEND_STRING(SS_TAP(X_B));
+        SEND_STRING(SS_UP(X_LALT)SS_UP(X_LSFT));
+      }
+      break;
+    case M_1PASS:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LALT)SS_TAP(X_1)SS_UP(X_LALT));
+          SEND_STRING(SS_DELAY(100));
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL));
+          SEND_STRING(SS_TAP(X_X));
+          SEND_STRING(SS_UP(X_LCTL)SS_UP(X_LSFT));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL));
+          SEND_STRING(SS_TAP(X_SPC));
+          SEND_STRING(SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+      }
+      break;
+    case M_HIDE:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LALT));
+          SEND_STRING(SS_TAP(X_MINS));
+          SEND_STRING(SS_UP(X_LALT));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_M));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+      }
+      break;
+    case M_MAX:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LALT));
+          SEND_STRING(SS_TAP(X_EQL));
+          SEND_STRING(SS_UP(X_LALT));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_A));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+      }
+      break;
+    case M_CLOSE:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LSFT));
+          SEND_STRING(SS_TAP(X_W));
+          SEND_STRING(SS_UP(X_LSFT)SS_UP(X_LCTL));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_K));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+      }
+      break;
+    case M_NDESK:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_RBRC));
+          SEND_STRING(SS_UP(X_LGUI));
+        } else {
+          SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_RGHT));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LCTL));
+        }
+      }
+      break;
+    case M_PDESK:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_LBRC));
+          SEND_STRING(SS_UP(X_LGUI));
+        } else {
+          SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_LEFT));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LCTL));
+        }
+      }
+      break;
+    case M_NTRM:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LCTL)SS_TAP(X_T)SS_UP(X_LCTL));
+        } else {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCTL));
+          SEND_STRING(SS_TAP(X_1));
+          SEND_STRING(SS_UP(X_LCTL)SS_UP(X_LSFT));
+        }
+      }
+      break;
+    case M_EMOJI:
+      if (record->event.pressed) {
+        if (m_is_chromebook) {
+          SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LGUI));
+          SEND_STRING(SS_TAP(X_SPC));
+          SEND_STRING(SS_UP(X_LGUI)SS_UP(X_LSFT));
+        } else {
+          SEND_STRING(SS_DOWN(X_LGUI)SS_TAP(X_SCLN)SS_UP(X_LGUI));
+        }
+      }
+      break;
+    case M_ISCROS:
+      if (record->event.pressed) {
+        m_is_chromebook = true;
+      }
+      break;
+    case M_ISWIN:
+      if (record->event.pressed) {
+        m_is_chromebook = false;
+      }
+      break;
+  }
+
+  return true;
+}
+
+bool caps_word_press_user(uint16_t keycode) {
+  switch (keycode) {
+    // Keycodes that continue caps word with shift applied.
+    case KC_A ... KC_Z:
+      // Apply shift to next key.
+      add_weak_mods(MOD_BIT(KC_LSFT));
+      return true;
+    // Keycodes that continue caps word without shifting.
+    case KC_1 ... KC_0:
+    case KC_BSPC:
+    case KC_DEL:
+    case KC_MINS:
+    case KC_UNDS:
+      return true;
+    // Deactivate caps word by default.
+    default:
+      return false;
+  }
+}
